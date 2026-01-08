95531e950cf959a9476d6159bcad3b4bbcd8f4fb
mohamedshawky982 <mohamedshawkym90@gmail.com>
2026-01-08 02:39:16 +0200

    remove unused vars

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,11 +1,6 @@
 import { I18nManager } from 'react-native';
 
 export const APP_NAME = I18nManager.isRTL ? 'فايد' : 'FAID';
-export const SUPABASE_URL = 'https://mmjgcacizlwcbrwgykbq.supabase.co';
-export const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tamdjYWNpemx3Y2Jyd2d5a2JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyNjI5MTIsImV4cCI6MjA2MTgzODkxMn0.IdjvDNin-WzJmhJmkDezuHlwkdqeCeOftJ0CfA0jvXw';
-export const GOOGLE_IOS_CLIENT_ID =
-  '190134150647-tnuvusuim08c3opnihreb37usikq0so9.apps.googleusercontent.com';
 
 export const FONT_FAMILY = 'Cairo';
 
